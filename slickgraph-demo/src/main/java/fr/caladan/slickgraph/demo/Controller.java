54bfde8f8ab00e58bc33b7d90b50ba3b924cfc5e
Remy Dautriche <remy.dautriche@caladan.fr>
2017-02-17 17:32:10 +0100

    Cleaned controller code of the java demo

--- slickgraph-demo/src/main/java/fr/caladan/slickgraph/demo/Controller.java
+++ slickgraph-demo/src/main/java/fr/caladan/slickgraph/demo/Controller.java
@@ -6,15 +6,11 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Slider;
 import javafx.scene.control.ToolBar;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
-import fr.caladan.slickgraph.SlickGraph;
 import fr.caladan.slickgraph.Timeseries;
 import fr.caladan.slickgraph.controller.SlickGraphController;
 import fr.caladan.slickgraph.dataloader.InMemoryTimeseriesLoader;
@@ -23,10 +19,6 @@ import fr.caladan.slickgraph.dataloader.InMemoryTimeseriesLoader;
  * Controller showing how to bind the slick graph component events
  */
 public class Controller extends SlickGraphController {
-
-	/** Root pane of the window */
-	// @FXML
-	// private AnchorPane root;
 
 	/** Toolbar containing the widgets */
 	@FXML
@@ -48,23 +40,12 @@ public class Controller extends SlickGraphController {
 	@FXML
 	private CheckBox showTimeCursorCheckBox;
 
-	/** Slick Graph widget */
-	// private SlickGraph slickGraph;
-
-	/** Horizontal coordinate of the last mouse event */
-	// protected double origMouseX;
-
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		super.initialize(location, resources);
 
 		// layout adjustment due to hidpi settings
 		slgContainer.heightProperty().addListener(e -> AnchorPane.setTopAnchor(slgContainer, toolBar.getHeight()));
-
-		/* slickGraph = new SlickGraph();
-		slickGraph.widthProperty().bind(root.widthProperty());
-		slickGraph.heightProperty().bind(root.heightProperty());
-		root.getChildren().add(slickGraph); */
 
 		// bind the slider value to the kernel bandwidth
 		smoothingSlider.setValue(slickGraph.getKernelBandWidth());
@@ -74,28 +55,6 @@ public class Controller extends SlickGraphController {
 		slickGraph.showShadingProperty().bind(showShadingCheckBox.selectedProperty());
 		slickGraph.showCurveProperty().bind(showCurveCheckBox.selectedProperty());
 		slickGraph.timeCursorVisibleProperty().bind(showTimeCursorCheckBox.selectedProperty());
-
-		// pick timeseries at the mouse position
-		// slickGraph.addEventHandler(MouseEvent.MOUSE_MOVED, e -> slickGraph.pickTimeseries(e.getX(), e.getY()));
-
-		// zoom in data on scroll
-		// slickGraph.addEventHandler(ScrollEvent.ANY, e -> slickGraph.zoom(e.getDeltaY()));
-
-		// save the position of the mouse when pressed
-		// slickGraph.addEventHandler(MouseEvent.MOUSE_PRESSED, e -> origMouseX = e.getSceneX());
-
-		// dragging the mouse performs a pan
-		/* slickGraph.addEventHandler(MouseEvent.MOUSE_DRAGGED, e -> {
-			slickGraph.pan(origMouseX - e.getSceneX());
-			origMouseX = e.getSceneX();
-		}); */
-
-		// feed the graph
-		/* List<Timeseries> timeseries = new ArrayList<Timeseries>();
-		timeseries.add(new Timeseries("my timeseries 1", Color.RED, DataGenerator.generateTimeseries(1000)));
-		timeseries.add(new Timeseries("my timeseries 2", Color.GREEN, DataGenerator.generateTimeseries(1000)));
-		timeseries.add(new Timeseries("my timeseries 3", Color.BLUE, DataGenerator.generateTimeseries(1000)));
-		slickGraph.setTimeseries(timeseries); */
 	}
 
 	/* (non-Javadoc)
